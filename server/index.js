172c8f8bf64305661451d75c23b2ba07fb587c09
cxp <fffg947221@qq.com>
2020-05-20 14:20:20 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = process.env.NODE_ENV !== 'production'
+
+async function start () {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const { host, port } = nuxt.options.server
+
+  await nuxt.ready()
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  app.get('/api/helloworld',function(req, res){
+    res.json({
+      msg: '你好',
+      status: 1
+    })
+  })
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
